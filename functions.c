125df3f93dc73ad24a4d8324e45be7e24ed1dd3e
kagwerior <kagwerior@gmail.com>
2023-01-26 18:50:06 -0800

    updated

--- functions.c
+++ functions.c
@@ -11,7 +11,8 @@
  * Return: Number of chars printed
  */
 
-int print_char(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_char(va_list types, char buffer[],
+		int flags, int width, int precision, int size)
 {
 	char c = va_arg(types, int);
 
--- main.h
+++ main.h
@@ -84,32 +84,4 @@ int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
 /*Function to print string in reverse*/
-int print_reverse(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
 
-/*Function to print a string in rot 13*/
-int print_rot13string(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
-
-/* width handler */
-int handle_write_char(char c, char buffer[],
-		int flags, int width, int precision, int size);
-int write_number(int is_positive, int ind, char buffer[],
-		int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision,
-		int length, char padd, char extra_c);
-int write_pointer(char buffer[], int ind, int length,
-		int width, int flags, char padd, char extra_c, int padd_start);
-
-int write_unsgnd(int is_negative, int ind, char buffer[],
-		int flags, int width, int precision, int size);
-
-/*** UTILS ***/
-int is_printable(char);
-int append_hexa_code(char, char[], int);
-int is_digit(char);
-
-long int convert_size_number(long int num, int size);
-long int convert_size_unsgnd(unsigned long int num, int size);
-
-#endif /* MAIN_H */
